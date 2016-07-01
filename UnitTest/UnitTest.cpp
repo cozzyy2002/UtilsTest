470d93144446ec597f87dd9f8b5df59215a100f4
cozzyy2002 <cozzyy2002@gmail.com>
2016-07-01 23:01:01 +0900

    Add log4cplus

--- UnitTest/UnitTest.cpp
+++ UnitTest/UnitTest.cpp
@@ -2,9 +2,12 @@
 //
 
 #include "stdafx.h"
+#include <log4cplus/configurator.h>
 
 int main(int argc, TCHAR** argv)
 {
+	log4cplus::PropertyConfigurator::doConfigure(LOG4CPLUS_TEXT("log4cplus.properties"));
+
 	::testing::InitGoogleTest(&argc, argv);
 	::testing::InitGoogleMock(&argc, argv);
 	return RUN_ALL_TESTS();
